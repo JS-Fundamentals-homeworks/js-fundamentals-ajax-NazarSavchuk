f5d99a33e0b3d34300f1e4dba240cf61dc5d80f7
Nazar Savchuk <nazarsavcuk3@gmail.com>
2026-03-19 22:38:36 +0100

    done task2

--- task2.js
+++ task2.js
@@ -5,3 +5,24 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.getElementById("userNameInput");
+const getUserButton = document.getElementById("getUserButton");
+const userCitySpan = document.getElementById("userCity");
+
+getUserButton.addEventListener("click", () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const foundUser = data.find((user) =>
+        user.name.toLowerCase().includes(userNameInput.value.toLowerCase()),
+      );
+      userCity.innerText = foundUser.address.city;
+    })
+    .catch((error) => {
+      userCitySpan.innerText =
+        "Something gone wrong :( But probably you entered wrong name, you need to try enter full name XD";
+      console.error(error);
+    });
+});
